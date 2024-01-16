41acd266c9057f563f00d66efff1a38b7ca181ac
vishwamani-banoth <156050957+vishwamani-banoth@users.noreply.github.com>
2024-01-16 14:02:45 +0530

    Delete uno_game directory

--- uno_game/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-#include "Simulator.h"
-using namespace std;
-int main(){
-    // Simulator sim=Simulator(2,4,9);
-    Simulator sim=Simulator(4,4,9);
-    sim.simulateAutomatically();
-}
